56bb4a5288b7f1e4cb4bdabde57326fffa8a80e7
Blessed-Samuel <samuelalisigwe22@gmail.com>
2023-12-15 04:58:57 -0800

    created files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- *  main - displays a specified phrase
- *  Return: 0 if exited successfully, non-zero otherwise
- */
+*main - displays a specified phrase
+*Return: 0 if exited successfully, non-zero otherwise
+*/
 
 int main(void)
 {
-        puts("Programming is like building a multilingual puzzle.\n");
-        return 0;
+	puts("Programming is like building a multilingual puzzle.\n");
+	return 0;
 }
-
